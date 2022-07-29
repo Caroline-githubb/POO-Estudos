dbaf650950e8ee222f32e7b40c4fb4a197bc1ee0
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-07-29 10:00:52 -0300

    Aulas 121 a 127

---
+++ CursoNelio/Ex-06/Entities/Order.cs
@@ -0,0 +1,19 @@
+using Ex_06.Entities.Enums;
+
+namespace Ex_06.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " +
+            Moment + ", " +
+            Status;
+
+        }
+    }
+}
---
+++ CursoNelio/Ex-06/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex_06.Entities;
+using Ex_06.Entities.Enums;
+
+Order order = new Order{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+//conversao de um tipo enumerado para string 
+string txt = OrderStatus.PendingPayment.ToString();
+
+//conversao de um tipo string para um tipo enumerado
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+Console.WriteLine(os);
+Console.WriteLine(txt);
